f3e55f3edc39c2e00ad73debb04cefb3941ec0d8
G0nz0 <155404796+g0nz0pera@users.noreply.github.com>
2024-05-03 12:18:45 -0300

    modifying th scope in the next.config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    //disable: process.env.NODE_ENV === "development",
-    disable: false,
-    scope: "/app",
-    workboxOptions: {
-        // Workbox options go here...
-        disableDevLogs: true
-    },
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  //disable: process.env.NODE_ENV === "development",
+  disable: false,
+  scope: "/",
+  workboxOptions: {
+    // Workbox options go here...
+    disableDevLogs: true,
+  },
 });
 
 const nextConfig = {};
